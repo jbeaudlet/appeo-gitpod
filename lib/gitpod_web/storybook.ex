875b301b48939e044e0223c959ac0a420709e4a6
jbb <3058338+BusinessAppsCo@users.noreply.github.com>
2023-04-11 19:40:05 +0000

    fixe(storybook): add default font to stories

--- lib/gitpod_web/storybook.ex
+++ lib/gitpod_web/storybook.ex
@@ -5,5 +5,5 @@ defmodule GitpodWeb.Storybook do
     # assets path are remote path, not local file-system paths
     css_path: "/assets/storybook.css",
     js_path: "/assets/storybook.js",
-    sandbox_class: "gitpod-web"
+    sandbox_class: "font-sans"
 end
